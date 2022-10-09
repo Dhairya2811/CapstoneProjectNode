6d6847280a9e4b144dbeac106e5fbd1ea9f5dc39
dhairya2711 <dsdrago27@gmail.com>
2022-10-09 02:49:13 -0800

    Payment Page loaded
    Able to reach till payment method. using the sessionStorage, able to transfer the list of items from cart to payment page. Still the UI for payment method is not done.

--- src/components/MyCart/MyCart.js
+++ src/components/MyCart/MyCart.js
@@ -1,13 +1,13 @@
-import { Component, useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import {useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Error404 from "../Error404/Error404";
 import ItemDisplay from "../ItemDisplay/ItemDisplay";
 import LoadingSpinner from "../LoadingComponent/Loading";
 
 var MyCart = ()=>{
     const [items, setItems] = useState([]);
-    const [empty, setEmpty] = useState("Loading...");
     const [loading, setLoading] = useState("true");
+    var navigate = useNavigate();
 
     useEffect(()=>{
         if(loading == "true"){
@@ -21,14 +21,22 @@ var MyCart = ()=>{
     });
     var renderItems = ()=>{
         if(items.length === 0){
-            if(loading == "false"){
-                setEmpty("The cart is empty")
-            }
-            return (<h2>{empty}</h2>);            
+            if(loading === "false"){
+                return (<h2>The cart is empty</h2>);  
+            }          
         }else{
             return (<>            
                 {items.map((item, index)=>{
-                    return <ItemDisplay item = {item} key={index} />
+                    return <>
+                        <div style={{width: "100%", display: "flex", justifyContent: "flex-end"}} >
+                        <button className="btn btn-success" onClick={()=>{checkout()}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16" style={{marginRight: "6px", marginTop: "-5px"}}>
+                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                            </svg>
+                            Checkout</button>
+                        </div>
+                        <ItemDisplay item = {item} key={index} />
+                    </>
                 })}
             </>);
         }
@@ -36,25 +44,18 @@ var MyCart = ()=>{
     var renderComponent = ()=>{
         if(sessionStorage.getItem("username") === null){
             return <Error404 errorMessage="You are not signed in." linkAvailable="true"/>
-        }else{
-            return (<>
-                {loading == "true" ? <LoadingSpinner /> : renderItems()}
-            </>);
         }
+            return (<>
+                {loading == "true" ? <LoadingSpinner /> : <>{renderItems()}</>}
+            </>);
     }
 
     var checkout = ()=>{
-        console.log(items);
+        sessionStorage.setItem("items", items)
+        navigate("/payment");
     }
 
     return(<>
-        <div style={{width: "100%", display: "flex", justifyContent: "flex-end"}} >
-                <button className="btn btn-success" onClick={()=>{checkout()}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16" style={{marginRight: "6px", marginTop: "-5px"}}>
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                    </svg>
-                    Checkout</button>
-        </div>
         <div className="displayitems">
         {renderComponent()}
     </div></>);
--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,9 +1,23 @@
-import { Component } from "react";
+import { useState } from "react";
+import Error404 from "../Error404/Error404";
 
 var Payment = ()=>{
-    <>
-        payment
-    </>
+    const [items, setItems] = useState([]);
+    if(sessionStorage.getItem("items")!= null){
+        setItems(sessionStorage.getItem("items"));
+        sessionStorage.removeItem("items");
+    }
+
+    var returnComponent = ()=>{
+        if(items.length != 0){
+            return (<></>);
+        }else{
+            return <Error404 errorMessage="Please Sign in to add item" linkAvailable = "true" />
+        }
+    }
+    return(<>
+        {returnComponent()}
+    </>);
 };
 
 export default Payment;
