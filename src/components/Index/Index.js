ff0b2de11f1965f7183c47022dd5101d616ccf55
dhairya2711 <dsdrago27@gmail.com>
2022-11-20 15:44:50 -0500

    added Ad section on the index page

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,3 +1,4 @@
+import Button from "react-bootstrap/Button";
 import React, {useEffect, useState} from "react";
 import { useLocation } from "react-router-dom";
 import ItemDisplay from "../ItemDisplay/ItemDisplay";
@@ -41,7 +42,29 @@ var  Index = ()=> {
         }
     }, [location]);
     return(
-        <div>
+        <div id="index_page">
+            {/* display ad */}
+            <div id="ad_div">
+                <div className="ad_video">
+                <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/FT3ODSg1GFE"+"?autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0"}></iframe>
+                </div>
+                <div className="ad_text">                
+                    <div>
+                        <div className="h1 ad_title">
+                            iPhone 14 Pro
+                        </div>
+                        <div className="ad_desc">
+                            A magical new way to interact with iPhone. Groundbreaking safety features designed to save lives. An innovative 48MP camera for mind-blowing detail. All powered by the ultimate smartphone chip.
+                        </div>
+                        <div className="ad_go_btn_div">
+                            <Button onClick={()=>{window.location.href = "/details/7"}} className="ad_go_btn">
+                                Go &#8594;
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* display all the items */}
             <div className="displayitems">
                 {loading == false ? (items).map((item, index)=>{
                     return <ItemDisplay item = {item} key={index} />
