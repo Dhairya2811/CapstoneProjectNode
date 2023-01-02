0a567e18c32ec04e58b19d89cec0dc3102a52929
dhairya2711 <dsdrago27@gmail.com>
2023-01-02 14:18:44 -0500

    added checkout button

--- src/components/MyCart/MyCart.js
+++ src/components/MyCart/MyCart.js
@@ -89,20 +89,29 @@ var MyCart = ()=>{
                         })}
                     </div>
                     <div className="displayPrice">
-                        <div className="itemList">
-                            {items.map(item=>{
-                                return <div><span className="title" style={{fontWeight: "bold"}}>{item.title}</span>: <span className="price">${parseFloat(item.price).toFixed(2)}</span></div>
-                            })}
-                        </div>
-                        <hr />
-                        <div className="subTotal">
-                            <span className="title"  style={{fontWeight: "bold"}}>Subtotal ({items.length} {items.length == 1 ? <span>item</span> : <span>items</span>})</span>:
-                            <span className="price">${getSubtotal().subtotal}</span>
+                        <div className="priceText">
+                            <div className="itemList">
+                                {items.map(item=>{
+                                    return <div><span className="title" style={{fontWeight: "bold"}}>{item.title}</span>: <span className="price">${parseFloat(item.price).toFixed(2)}</span></div>
+                                })}
+                            </div>
+                            <hr />
+                            <div className="subTotal">
+                                <span className="title"  style={{fontWeight: "bold"}}>Subtotal ({items.length} {items.length == 1 ? <span>item</span> : <span>items</span>})</span>:
+                                <span className="price">${getSubtotal().subtotal}</span>
+                            </div>
                         </div>
 
                         {/* paypal button */}
-                        <div id="paymentButtons">                    
-                            
+                        <div className="cartPaymentButtonDiv">                    
+                            {/* <div style={{width: "100%", display: "flex", justifyContent: "flex-end"}} > */}
+                            <button className="btn btn-primary cartPaymentButton" onClick={()=>{checkout()}}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16" style={{marginRight: "6px", marginTop: "-5px"}}>
+                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                                </svg>
+                                <span className="text">Checkout</span>
+                            </button>
+                            {/* </div>  */}
                         </div>
                     </div>
                 </div>
